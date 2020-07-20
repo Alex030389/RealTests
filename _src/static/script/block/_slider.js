fb37277ab2eca32f01b542538cc4789ee77fd055
Alex Ka <x182838@gmail.com>
2020-07-20 17:30:26 +0300

    pages: all-vendors, testimonials

--- _src/static/script/block/_slider.js
+++ _src/static/script/block/_slider.js
@@ -20,6 +20,7 @@ $('[data-slider="1"]').slick({
 	infinite: true,
 	accessibility: false,
 	touchThreshold: 10,
+	swipeToSlide: true,
 	responsive: [
 		{
 			breakpoint: 1200,
@@ -40,3 +41,35 @@ $('[data-slider="1"]').slick({
 });
 
 $('[data-slider="1"] button').text('');
+
+//////////////////////////////////////////// data-slider="1"
+$('[data-slider="2"]').slick({
+	slidesToShow: 6,
+	// slidesToScroll: 1,
+	arrows: false,
+	// infinite: false,
+	dots: true,
+	accessibility: false,
+	touchThreshold: 20,
+	swipeToSlide: true,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				variableWidth: true
+			}
+		},
+		{
+			breakpoint: 576,
+			settings: {
+				variableWidth: false,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				infinite: false,
+				swipeToSlide: false,
+			},
+		}
+	]
+});
+
+// $('[data-slider="1"] button').text('');
