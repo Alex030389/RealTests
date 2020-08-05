7ccff7fb4aa36336d6ba6ac1512a7bc9fca76843
AlexKa <x182838@gmail.com>
2020-08-05 18:30:44 +0300

    change link

--- _src/static/script/main.js
+++ _src/static/script/main.js
@@ -2,22 +2,10 @@
 
 svg4everybody();
 
-// polifill
+// ///////////////////////////////////// polifill
 objectFitImages();
 
-// ///////////////////////////////////// style
-$('button	').on('click', function() {
-	$(this).blur();
-});
-
-$('select').on('change', function (){
-	let color = $(this).find('option').css('color');
-	$(this).css('color', color);
-});
-
-$('.all-vendors__letter').prev(".all-vendors__wrap-link").css({"margin-bottom": "23px"});
-
-// block
+// ///////////////////////////////////// block
 @@include('block/_hamburger.js')
 @@include('block/_mobile-sub-nav.js')
 @@include('block/_header-search.js')
@@ -27,3 +15,23 @@ $('.all-vendors__letter').prev(".all-vendors__wrap-link").css({"margin-bottom": 
 @@include('block/_show-password.js')
 @@include('block/_scroll-top.js')
 @@include('block/_stick-footer.js')
+
+// ///////////////////////////////////// ather
+// remove focus from all button
+$('button	').on('click', function() {
+	$(this).blur();
+});
+
+// change option color
+$('select').on('change', function (){
+	let color = $(this).find('option').css('color');
+	$(this).css('color', color);
+});
+
+// add margin bottom all-vendors__wrap-link
+$('.all-vendors__letter').prev(".all-vendors__wrap-link").css({"margin-bottom": "23px"});
+
+
+$('[data-js="mod-help-title"]').on('click', function() {
+		$(this).next().slideToggle(200);
+});
