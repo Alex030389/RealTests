ac81e3a0c8a5d48d57a3b1c9cfee0f101a0a6d34
AlexKa <x182838@gmail.com>
2020-09-05 19:40:22 +0300

    page: invoice

--- _src/static/script/block/_header-search.js
+++ _src/static/script/block/_header-search.js
@@ -9,11 +9,11 @@ $('[data-btn="open-search"]').on('click', function() {
 	} else {
 		$('[data-js="main-search"]').slideUp(150);		
 	}
-})
+});
 
 $('[data-btn="search-close"]').on('click', function() {
 	$('[data-js="main-search"]').hide();
-})
+});
 
 $('body').mouseup(function(even) {
   if($('[data-js="main-search"]').has(even.target).length === 0) {
